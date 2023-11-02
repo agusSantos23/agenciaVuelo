dbe879fff1ac79a94a84f7eb509719994d2df0ed
Agus <agustindelossatons023@gmail.com>
2023-11-02 17:37:27 +0100

    Base

---
+++ Vigo/script.js
@@ -0,0 +1,16 @@
+const elemento = document.querySelector('.Back');
+const elementoVolver = document.getElementById('volver');
+
+
+function activarVuelo(){
+
+    elemento.style.animationName = 'vuelo';
+    elemento.style.animationDuration = '3s';
+    elemento.style.animationTimingFunction = 'linear';
+}
+
+
+
+
+elementoVolver.addEventListener('click', () =>{setTimeout(() => {window.history.back()}, 2301);setTimeout(activarVuelo, 300);})
+
